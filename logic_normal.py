6018fe0d2e57ee050e669440da15b82cc4d097fc
soju6jan <cybersol@naver.com>
2020-03-21 03:52:09 +0900

    1

--- logic_normal.py
+++ logic_normal.py
@@ -43,7 +43,7 @@ class LogicNormal(object):
                     url = '%s/%s/api/add_download?id=%s' % (SystemModelSetting.get('ddns'), package_name, ret.id)
                     if SystemModelSetting.get_bool('auth_use_apikey'):
                         url += '&apikey=%s' % SystemModelSetting.get('auth_apikey')
-                    if SystemModelSetting.get('ddns').startswith('https://sjva-dev'):
+                    if app.config['config']['is_sjva_server']:
                         msg += '\n' + ret.magnet + '\n'
                     else:
                         msg += '\n➕ 다운로드 추가\n<%s>\n' % url
